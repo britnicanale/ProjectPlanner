355fb71e9f38536bcf8a8cd7f95d50470ea10e7b
Britni <bcanale@stuy.edu>
2020-06-01 23:32:02 -0400

    Added register and login page

--- projectplanner/app.py
+++ projectplanner/app.py
@@ -16,6 +16,10 @@ def welcome():
 def login():
     return render_template("login.html")
 
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
