4c9e90754fa035570d619b9e14006946c181cc2b
Britni <bcanale@stuy.edu>
2020-06-02 00:22:09 -0400

    Fixed home page buttons.

--- projectplanner/app.py
+++ projectplanner/app.py
@@ -5,18 +5,32 @@
 
 
 from flask import Flask, render_template, session, request, url_for, redirect, flash
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ['POST', "GET"])
 def welcome():
+    if 'username' not in session:
+        if "uname" not in request.form:
+            return redirect(url_for("login"))
     return render_template("temp.html")
 
-@app.route("/login")
+@app.route("/login", methods = ['POST', "GET"])
 def login():
+    if 'username' in session:
+        flash("You are already logged in")
+        return redirect(url_for("welcome"))
     return render_template("login.html")
 
-@app.route("/register")
+@app.route("/logout")
+def logout():
+    if 'username' in session:
+        flash("You have successfully logged out")
+        session.clear()
+    return redirect(url_for("login"))
+
+@app.route("/register", methods = ["GET"])
 def register():
     return render_template("register.html")
 
